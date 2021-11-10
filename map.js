ec6dc82371cefb78e608459e723a0d124e4739d7
Juan J Galdo Mac <juanjorgegaldo@gmail.com>
2021-11-11 00:16:05 +0100

    basic chart in canvas

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 const nomRuta = document.getElementById('nomRuta');
 const rutas = document.getElementById('rutas');
 const showBtn = document.getElementById('show');
-const elevation = document.getElementById('elevation');
+//const elevation = document.getElementById('elevation');
 
 /* Painting the map */
 
@@ -68,6 +68,10 @@ showBtn.onclick = (e) =>{
 			rutas.value = 0;
 			break;
 		case '4':
+			gpx = './tracks/LUGO_ALMERIA.gpx';
+			rutas.value = 0;
+			break;
+		case '5':
 			gpx = './tracks/LUGO_SANT_PORTUGAL_CADIZ.gpx';
 			rutas.value = 0;
 			break;
@@ -87,7 +91,43 @@ showBtn.onclick = (e) =>{
 		mymap.fitBounds(e.target.getBounds());
 		nomRuta.textContent="Ruta: "+e.target._info.name+" || Distancia: "+((e.target._info.length)/1000).toFixed(2)+" Kms || Desnivel: "+((e.target._info.elevation.gain)).toFixed(2)+" mts";
 		console.log(e.target._info);
-	}).addTo(mymap);	
+	}).addTo(mymap);
+
+	const ctx = document.getElementById('elevation').getContext('2d');
+	const myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
 	
 }
 
